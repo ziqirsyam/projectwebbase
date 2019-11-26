8eae32e32fd268565634004a803cdfd3555fd4f3
acit1199 <58203145+acit1199@users.noreply.github.com>
2019-11-26 20:21:00 +0800

    Update index.php

--- index.php
+++ index.php
@@ -9,7 +9,17 @@
 	<?php include 'footer.php'?>
 </body>
 </html>
-<!--
+<!--    References
+	https://shopee.com.my/
+	https://www.lazada.com.my/
+	https://www.ebay.com.my/
+	https://www.alibaba.com/
+	testnewgithubReferences
+	https://shopee.com.my/
+	https://www.lazada.com.my/
+	https://www.ebay.com.my/
+	https://www.alibaba.com/
+	testnewgithub
 	References
 	https://shopee.com.my/
 	https://www.lazada.com.my/
