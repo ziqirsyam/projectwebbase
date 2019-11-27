8c1c6cd0433bfa82a81370e1c7768555560703fd
warniberjaya <58213344+warniberjaya@users.noreply.github.com>
2019-11-28 01:12:13 +0800

    update generalnavbar.php

--- generalnavbar.php
+++ generalnavbar.php
@@ -3,6 +3,37 @@
 	<a href="sign-up.php">Sign-up</a>
 	<a href="sign-in.php">Sign-in</a>	
 </div>
+<div class="navitop">
+		<a href="#home"> Welcome user</a>
+		<div class="cart">
+			<a href="#cart">Cart</a></div>
+		<div class="signin">
+			<a href="signin.html">Sign In</a></div>
+		<div class="signup">
+			<a href="signup.html">Sign Up</a></div>
+	</div>
+
+	<!--side navigation-->
+	<section>
+		<nav>
+			<ul>
+				<li><a href="userdetail.html">User Detail</a></li>
+				<li><div class="dropdown">
+					<button class="dropbtn">Category
+						<i class="fa fa-caret-down"></i>
+					</button>
+					<div class="dropdown_content">
+						<a href="Laptop.html">Computer</a>
+						<a href="Smartphone.html">Smartphone</a>
+						<a href="Tablet.html">Tablet</a>
+						<a href="Smartwatch.html">Smart Watch</a>
+						<a href="Accessories.html">Accessories</a>
+					</div>
+					</div>
+				</li>
+			</ul>
+		</nav>
+	</section>
 <!--
 	https://www.w3schools.com/howto/howto_js_topnav.asp
 	https://www.w3schools.com/howto/howto_js_topnav_responsive.asp
