a57ae2024ac53075d4a31d9f52e65cb6c6aaed6b
warniberjaya <58213344+warniberjaya@users.noreply.github.com>
2019-11-27 13:41:26 +0800

    update signup.php

--- sign-up.php
+++ sign-up.php
@@ -1,24 +1,79 @@
 <!DOCTYPE html>
-<html>
+<html lang="eng>
 <head>
+	<meta charset="utf-8">  
 	<title>Sign-Up</title>
 </head>
 <body>
 	<?php include 'generalnavbar.php'?>
-	<h1>Sign-Up Page</h1>
-	<form action="">
-		<div>
-			<h2>Sign Up Form</h2>
-			<hr>
-			<label for="fname">First Name :</label>
-			<input type="text" name="fname" placeholder="Enter your first name..." required>
-		</div>
-		<div>
-			<button type="reset">Reset</button>
-			<button type="submit">Sign-up</button>
-		</div>
-	</form>
-	<?php include 'footer.php'?>
+	
+	<div class="form">
+  
+	<h1> Sign Up</h1>
+	<p>Please fill in this form</p><br>
+		<form action="">		
+		<label for="fname"><b>First name</b></label>
+		<input type="text"placeholder="First Name"name="fname"/>
+		<br>
+		<label for="lname"><b>Last name</b></label>
+		<input type="text"placeholder="Last Name"name="lname"/>
+		<br>
+		<label for="username"><b>Username</b></label>
+		<input type="text"placehlder="username"name="uname"/>
+		<br>
+		<label for="email"><b>Email</b></label>
+		<input type="text"placeholder="email id"name="email"required />
+		<br>
+		<label for="psw"><b>Password</b></label>
+		<input type="password"placeholder="password"name="psw" required />
+		<br>
+		<label for="conpsw"><b>Confirm Password</b></label>
+		<input type="password"placeholder="Confirm password"name="conpsw"required/>
+		<br>
+		<label for="Gender"><b>Gender:</b></label>
+		<input class="gender" type="radio"name="genders" value="Male"required> Male
+		<input class="gender"type="radio"name="genders"value="Female"required> Female<br>
+		<br>
+		<label for="state"><b>State: </b></label>
+		<select name="state">
+		<option value="1">(Please select a State)</option>
+		<option value="KUL">Kuala Lumpur</option>
+		<option value="LBN">Labuan</option>
+		<option value="PJY">Putrajaya</option>
+		<option value="JHR">Johor</option>
+		<option value="KDH">Kedah</option>
+		<option value="KTN">Kelantan</option>
+		<option value="MLK">Melaka</option>
+		<option value="NSN">N.Sembilan</option>
+		<option value="PHG">Pahang</option>
+		<option value="PRK">Perak</option>
+		<option value="PLS">Perlis</option>
+		<option value="PNG">P.Pinang</option>
+		<option value="SBH">Sabah</option>
+		<option value="SWK">Sarawak</option>
+		<option value="SGR">Selangor</option>
+		<option value="TRG">Terengganu</option>
+		</select>
+		<br><br>
+		<p>
+        <textarea name="terms" cols="40" rows="2"
+        readonly="readonly">TERMS AND CONDITIONS...</textarea>
+        </p>
+		<label><input type="checkbox"checked name="accept">I ACCEPT THE ABOVE TERM AND CONDITIONS</label>
+
+		<div class="clearfix">
+			<button type="reset" name="resetbtn" class="button"value="reset the form">Clear</button>
+			<button type="submit"name="submit"class="button"value="submited">Register<a href="userpage.html"></button></a>
+
+		<p class="message">Already Registered? <a href="index.html">Homepage</a></p>
+
+	    </div>
+	    </form>
+
+	   </div>
+   </div>
+
+<?php include 'footer.php'?>
 </body>
 </html>
 <!--
