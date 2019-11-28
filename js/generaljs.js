9a8e1b7cf759e8b0ac497dfebe07ba7eeb0114ca
AiniWafa <58218569+AiniWafa@users.noreply.github.com>
2019-11-28 14:42:51 +0800

    Update generaljs.js

--- js/generaljs.js
+++ js/generaljs.js
@@ -2,3 +2,23 @@
 	Reference
 	https://www.w3schools.com/js/default.asp
 */
+
+function validate()
+{
+	if( document.userform.username.value == "" )
+	{
+		alert( "Please provide your Username!" );
+		document.userform.username.focus() ;
+		return false;
+	}
+
+	if( document.userform.password.value == "" )
+	{
+		alert( "Please provide your Password!" );
+		document.userform.Emailaddress.focus() ;
+		return false;
+	}
+
+ alert("The form was successfully submitted")
+ return( true );
+}
