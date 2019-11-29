5ad848bc95e7c3a9970c750b2ee073379e7aad6b
AiniWafa <58218569+AiniWafa@users.noreply.github.com>
2019-11-29 20:47:36 +0800

    Update cart.php

--- user/cart.php
+++ user/cart.php
@@ -2,32 +2,84 @@
 <html>
 <head>
 	<title>Cart</title>
+	<script src="https://code.jquery.com/jquery-2.2.4.js" charset="utf-8"></script>
 </head>
 <body>
 	<?php include'usernavbar.php'?>
-	<h1>Cart</h1>
-	<div>
-		<table>
-			<tr>
-				<th>Item</th>
-				<th>Price</th>
-				<th>Action</th>
-			</tr>
-			<tr>
-				<td>test1</td>
-				<td>RM 999</td>
-				<td><button>Remove</button></td>
-			</tr>
-		</table>
-	</div>
-	<div>
-		<table>
-			<tr>
-				<th>Total price : RM 999</th>
-				<td><a href="checkout.php"><button>Checkout</button></a></td>
-			</tr>
-		</table>
-	</div>
+	
+
+	<div class="shopping-cart">
+
+      <h1>Cart</h1>
+
+      <!-- Product 1 -->
+      <div class="item">
+        <div class="buttons">
+          <span class="deletebutton"></span>
+          <span class="likebutton"></span>
+        </div>
+
+        <div class="image">
+          <img src=".jpg" alt="" />
+        </div>
+
+        <div class="description">
+          <span>phone</span>
+          <span>iphone11</span>
+          <span>White</span>
+        </div>
+
+        <div class="quantity">
+          <button class="plusbutton" type="button" name="button">
+            <img src="plus.svg" alt="" />
+          </button>
+          <input type="text" name="name" value="1">
+          <button class="minusbutton" type="button" name="button">
+            <img src="minus.svg" alt="" />
+          </button>
+        </div>
+
+        <div class="totalprice">RM1549</div>
+        <a href="checkout.php"><button>Checkout</button>
+    </div>
+
+	<script type="text/javascript">
+      $('.minusbutton').on('click', function(e) {
+    		e.preventDefault();
+    		var $this = $(this);
+    		var $input = $this.closest('div').find('input');
+    		var value = parseInt($input.val());
+
+    		if (value > 1) {
+    			value = value - 1;
+    		} else {
+    			value = 0;
+    		}
+
+        $input.val(value);
+
+    	});
+
+    	$('.plusbuttonn').on('click', function(e) {
+    		e.preventDefault();
+    		var $this = $(this);
+    		var $input = $this.closest('div').find('input');
+    		var value = parseInt($input.val());
+
+    		if (value < 100) {
+      		value = value + 1;
+    		} else {
+    			value =100;
+    		}
+
+    		$input.val(value);
+    	});
+
+      $('.likebutton').on('click', function() {
+        $(this).toggleClass('is-active');
+      });
+    </script>
+
 	<?php include '../footer.php'?>
 </body>
 </html>
