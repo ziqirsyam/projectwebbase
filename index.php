cdb250946a784dfeb56a379e1d4e34990e30fe1e
acit1199 <58203145+acit1199@users.noreply.github.com>
2019-11-30 22:18:40 +0800

    Update index.php

--- index.php
+++ index.php
@@ -1,11 +1,13 @@
-<!DOCTYPE html>
-<html>
-<header><!--top navigation-->
+<?php include 'generalnavbar.php'?>
+	<!--<h1>Index - Page</h1>-->
+	<?php include 'footer.php'?>
+	<header><!--top navigation-->
+		<script src="https://kit.fontawesome.com/a076d05399.js"></script>
 		<title>HomePage</title>
 		<link rel="stylesheet" type="text/css" href="indexdeis.css">
-	</header>
-	<nav>
 
+
+	</header>
 	<!--side navigation-->
 	<section>
 		<nav>
@@ -13,19 +15,21 @@
 		<div class="flex-container">
 			
 		    <li class="dropdown">
-		     <a class="dropbtn">Dropdown</a>
+		    <i  id="bars" class="fas fa-bars"></i>
 		    <div class="dropdown-content">
-			<a href="#">Sign Up</a>
-     		<a href="#">Log in</a>
-		</div></li> 
+			<a class="sign-up" href="#">Sign Up</a>
+     		<a class="log-in" href="#">Log in</a>
+		</div></li>
 		<li class="list"><a href="#Mistore">Mi Store</a></li>
   			<li class="list"><a href="#news">Apple</a></li>
   			<li class="list"><a href="#contact">Samsung</a></li>
   			<li class="list"><a href="#about">Sony</a></li>
   			<li class="list"><a href="#about">Xiaomi</a></li>
   			<li class="list"><a href="#about">Oppo</a></li>
-  			<li class="list"><a href="#about">Cart</a></li>
-  		<form action="/action_page.php">
+  			<div class="cart">
+  			<li class="list"><a href="#about"><i class="fas fa-shopping-cart"></i></a></li>
+  		</div>
+  		<form  class="search" action="/action_page.php">
   			<li class="Search"><a href="#about"><input type="text" name="search" placeholder="Search.."></a></li>
   		</form>
 
@@ -34,13 +38,16 @@
 	</ul>
 		</nav>
 	</section>
+	<body>
 	<article>
+	<div class="link">
 	<div class="flex-container">
-			<div class="board"><a href="Apple.html"><img src="">Learn more</a><br></div>
+			<div class="board"><a href="Apple.html"><img src="">Learn more</a> <br></div>
 			<div class="Samsung"><a href="Samsung.html"><img src="">Learn more</a><br></div>
 			<div class="Sony"><a href="Sony.html"><img src="">Learn more</a><br></div>
 			<div class="Xiaomi"><a  href="Xiaomi.html"><img src="">Learn more</a><br></div>
 			<div class="Oppo"><a href="Oppo.html"><img src="">Learn more</a><br></div>
+		</div>
 	</div>
 	<script>
 		window.onscroll = function() {myFunction()};
@@ -62,6 +69,7 @@
 	</footer>
 </body>
 </html>
+<!--    References
 <!--    References
 	https://shopee.com.my/
 	https://www.lazada.com.my/
