b934585d64c88527624cad50924929137db20191
warniberjaya <58213344+warniberjaya@users.noreply.github.com>
2019-12-04 15:00:08 +0800

    Update xiaomi.php

--- brand/xiaomi.php
+++ brand/xiaomi.php
@@ -2,6 +2,11 @@
 <html>
 <head>
 	<title>Xiaomi</title>
+	<article>
+	<div class="flex-container">
+			<div class="pocophone"><a href="pocophone.php"><img src="">Pocophone</a><br></div>
+	</div>
+</article>
 </head>
 <body>
 
