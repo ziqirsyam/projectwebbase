2322d9c8310d0c4317a3ad54ece1ee73c50d1fed
warniberjaya <58213344+warniberjaya@users.noreply.github.com>
2019-12-04 14:58:44 +0800

    Update sony.php

--- brand/sony.php
+++ brand/sony.php
@@ -2,6 +2,14 @@
 <html>
 <head>
 	<title>Sony</title>
+	<article>
+	<div class="flex-container">
+			<div class="xperiap"><a href="xperiap.php"><img src="">Sony Xperia P</a><br></div>
+			<div class="xperiax"><a href="xperiax.php"><img src="">Sony Xperia X </a><br></div>
+			<div class="xperiaxz"><a href="xperiaxz.php"><img src="">Sony Xperia XZ Premium </a><br></div>
+	</div>
+</article>
+</body>
 </head>
 <body>
 
