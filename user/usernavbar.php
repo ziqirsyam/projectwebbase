e15fe91de3d505d36a4c0f8d523effc0fbc9e0e1
warniberjaya <58213344+warniberjaya@users.noreply.github.com>
2019-12-04 15:06:28 +0800

    Update usernavbar.php

--- user/usernavbar.php
+++ user/usernavbar.php
@@ -1,38 +1,34 @@
-	<header><!--top navigation-->
-	<div class="head1">MI Store</div>
-	<div class="head2">Welcome to MiStore </div>
+<header><!--top navigation-->
+		<title>HomePage</title>
+		<link rel="stylesheet" type="text/css" href="indexdeis.css">
 	</header>
-
-	<div class="navitop">
-		<a href="#home">username</a>
-		<div class="cart">
-			<a href="#cart">Cart</a></div>
-		<div class="signin">
-			<a href="signup.html">Sign up</a></div>
-		<div class="signout">
-			<a href="index.html">Sign Out</a></div>
-	</div>
-
+	<nav>
 	<!--side navigation-->
 	<section>
 		<nav>
-			<ul>
-				<li><a href="userdetail.html">User Detail</a></li>
-				<li><div class="dropdown">
-					<button class="dropbtn">Category
-						<i class="fa fa-caret-down"></i>
-					</button>
-					<div class="dropdown_content">
-						<a href="Laptop.html">Computer</a>
-						<a href="Smartphone.html">Smartphone</a>
-						<a href="Tablet.html">Tablet</a>
-						<a href="Smartwatch.html">Smart Watch</a>
-						<a href="Accessories.html">Accessories</a>
-					</div>
-					</div>
-					<li><a href="sellitem.html">Sell Item</a></li>
-				</li>
-			</ul>
+	<ul id="navitop" class ="navitop">
+		<div class="flex-container">
+			
+		    <li class="dropdown">
+		     <a class="dropbtn">More</a>
+		    <div class="dropdown-content">
+		    <a href="userdetails.php">User Details</a>
+			<a href="index.php">Sign Out</a>
+
+		</div></li> 
+		<li class="list"><a href="#Mistore">Mi Store</a></li>
+  			<li class="list"><a href="userapple.php">Apple</a></li>
+  			<li class="list"><a href="usersamsung.php">Samsung</a></li>
+  			<li class="list"><a href="usersony.php">Sony</a></li>
+  			<li class="list"><a href="userxiomi.php">Xiaomi</a></li>
+  			<li class="list"><a href="useroppo.php">Oppo</a></li>
+  			<li class="list"><a href="usercart.php">Cart</a></li>
+  		<form action="/action_page.php">
+  			<li class="Search"><a href="#about"><input type="text" name="search" placeholder="Search.."></a></li>
+  		</form>
+  				
+  	</div>
+	</ul>
 		</nav>
 	</section>
 <!--
