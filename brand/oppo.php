41995417aa77c9168c8cbcaaed67f342a1548fcf
warniberjaya <58213344+warniberjaya@users.noreply.github.com>
2019-12-04 14:56:10 +0800

    update oppo.php

--- brand/oppo.php
+++ brand/oppo.php
@@ -2,6 +2,13 @@
 <html>
 <head>
 	<title>Oppo</title>
+	<article>
+	<div class="flex-container">
+			<div class="oppof1s"><a href="oppof1s.php"><img src="">Oppo F1s</a><br></div>
+			<div class="oppoa37"><a href="oppoa37.php"><img src="">Oppo A37</a><br></div>
+	</div>
+</article>
+	
 </head>
 <body>
 
