d722d60b7d3b08ed01cc9e43fa1c916019efda47
warniberjaya <58213344+warniberjaya@users.noreply.github.com>
2019-12-06 03:59:56 +0800

    Update homepageadmin.php

--- admin/homepageadmin.php
+++ admin/homepageadmin.php
@@ -2,58 +2,59 @@
 <html>
 <head>
 	<title>Homepage</title>
+	<link rel="stylesheet"type="text/css"href="adminstyle.css"/>
 </head>
 <body>
 	<?php include 'adminnavbar.php'?>
 
+<div id="header"><br><br>
+	<div class="adminlogo"><a href="#">Admin<span>Dashboard</span></a></div><br><br><br>
+</div>
+
+<div id="container">
+<div id="sidebar">
+	<ul id="nav">
+		<li><a class="dash"href="adminhomepage.php">Dashboard</a></li>
+		<li><a href="additem.php">Add Data</a></li>
+		<li><a href="deleteitem.php">Delete Data</a></li>
+		<li><a href="updateitem.php">Update Data</a></li>
+		<li><a href="summaryitem.php">Summary Data</a></li>
+		<li><a href="daffa.php">Developer Email</a></li>
+		<li><a href="signinadmin">Log Out</a></li>
+	</ul>
+</div>
 
 <div id="content">
-	<h2>  Data </h2>
-	<p class="ID">ID</p>
-		<table ><!--Change to PHP-->
-					<tr>
-						<th>Name:</th>
-						<td> user name</td>
-					</tr>
-					<tr>	
-					    <th>Username:</th>
-					    <td>php</td>
-					</tr>    
-					<tr>    
-				        <th>Email:</th>
-				        <td> php</td>
-				    </tr> 
-				    <tr>
-				    	<th>No.Phone</th>
-				    	<td><i>php</i></td>
-				    </tr>
-				    <tr>
-				    	<th>Gender</th>
-				    	<td><i>php</i></td>
-				    </tr>
-				     <tr>
-				    	<th>Address</th>
-				    	<td>php</td>
-				    </tr>
-				     <tr>
-				    	<th>Post Code</th>
-				    	<td>php</td>
-				    </tr>
-				    <tr>
-				    	<th>State</th>
-				    	<td>php</td>
-				    </tr>
-				    <tr>
-				        <th>Country</th>
-				        <td>php</td>
-				    </tr>    	
-				</table>
+	<h1>  Dashboard </h1><br>
+	<p>Details</p><br>
 
+	<div id="info">
+		<div class="info-one">Info </div>
+		<div class="info-panel">Php </div>
+		
+	</div>
+
+	<div id="info">
+		<div class="info-one">Info </div>
+		<div class="info-panel">Php </div>
+		
+	</div>
+
+	<div id="info">
+		<div class="info-one">Info </div>
+		<div class="info-panel">Php </div>
+		
 	</div>
 
 
 
+
+
 </div>
+</div>
+<footer>mi store</footer>
+</body>
+</html>
 	<?php include '../footer.php'?>
 </body>
 </html>
