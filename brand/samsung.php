6bb9c7edad4830f2622997586269a3e57a647231
acit1199 <58203145+acit1199@users.noreply.github.com>
2019-12-06 18:28:42 +0800

    Update samsung.php

--- brand/samsung.php
+++ brand/samsung.php
@@ -2,16 +2,47 @@
 <html>
 <head>
 	<title>Samsung</title>
-	
+	<link rel="stylesheet" type="text/css" href="branddeis.css">
+	<script src="https://kit.fontawesome.com/a076d05399.js"></script>
 </head>
 <body>
+	<nav>
+	<!--side navigation-->
+	<section>
+		<nav>
+	<ul id="navitop" class ="navitop">
+		<div class="flex-container">
+			
+		    <li class="dropdown">
+		    <i  id="bars" class="fas fa-bars"></i>
+		    <div class="dropdown-content">
+			<a class="sign-up" href="#">User detail</a>
+     		<a class="log-in" href="#">Sign Out</a>
+		</div></li>
+		<li class="list"><a href="#Mistore">Mi Store</a></li>
+  			<li class="list"><a href="userapple.php">Apple</a></li>
+  			<li class="list"><a href="usersamsung.php">Samsung</a></li>
+  			<li class="list"><a href="usersony.php">Sony</a></li>
+  			<li class="list"><a href="userxiomi.php">Xiaomi</a></li>
+  			<li class="list"><a href="useroppo.php">Oppo</a></li>
+  			<div class="cart">
+  			<li class="list"><a href="#about"><i class="fas fa-shopping-cart"></i></a></li>
+  		</div>
+  		<form  class="search" action="/action_page.php">
+  			<li class="Search"><a href="#about"><input type="text" name="search" placeholder="Search.."></a></li>
+  		</form>
+  				
+  	</div>
+	</ul>
+		</nav>
 <article>
-	<div class="flex-container">
-			<div class="samsungs9"><a href="samsungs9.php"><img src="">Samsung Galaxy S9</a><br></div>
-			<div class="samsungnote9"><a href="samsungnote9.php"><img src="">Samsung Galaxy S9</a><br></div>
-			<div class="samsungs10+"><a href="samsungs10+.php"><img src="">Samsung Galaxy S10+</a><br></div>
-			<div class="samsungs8"><a  href="samsungs8.php"><img src="">Samsung Galaxy S8</a><br></div>
-			<div class="samsungnote3"><a href="samsungnote3.php"><img src="">Samsung Galaxy Note 3</a><br></div>
+	<p class="brand">Samsung Product</p>
+	<div class="flex-container2">
+			<div class="samsungs9"><p>Samsung Galaxy S9</p><a href="samsungs9.php"><img src="">Buy</a><br></div>
+			<div class="samsungnote9"><p>Samsung Galaxy Note 9</p><a href="samsungnote9.php"><img src="">Buy</a><br></div>
+			<div class="samsungs10"><p>Samsung Galaxy S10+</p><a href="samsungs10+.php"><img src="">Buy</a><br></div>
+			<div class="samsungs8"><p>Samsung Galaxy S8</p><a  href="samsungs8.php"><img src="">Buy</a><br></div>
+			<div class="samsungnote3"><p>Samsung Galaxy Note 3</p><a href="samsungnote3.php"><img src="">Buy</a><br></div>
 	</div>
 </article>
 </body>
