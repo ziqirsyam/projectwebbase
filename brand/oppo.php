8bf948bfc26c4fd9125be9ccabc924018c5aed35
acit1199 <58203145+acit1199@users.noreply.github.com>
2019-12-06 18:26:46 +0800

    Update oppo.php

--- brand/oppo.php
+++ brand/oppo.php
@@ -2,12 +2,44 @@
 <html>
 <head>
 	<title>Oppo</title>
+	<link rel="stylesheet" type="text/css" href="branddeis.css">
+	<script src="https://kit.fontawesome.com/a076d05399.js"></script>
 </head>
 <body>
+		<nav>
+	<!--side navigation-->
+	<section>
+		<nav>
+	<ul id="navitop" class ="navitop">
+		<div class="flex-container">
+			
+		    <li class="dropdown">
+		    <i  id="bars" class="fas fa-bars"></i>
+		    <div class="dropdown-content">
+			<a class="sign-up" href="#">User detail</a>
+     		<a class="log-in" href="#">Sign Out</a>
+		</div></li>
+		<li class="list"><a href="#Mistore">Mi Store</a></li>
+  			<li class="list"><a href="userapple.php">Apple</a></li>
+  			<li class="list"><a href="usersamsung.php">Samsung</a></li>
+  			<li class="list"><a href="usersony.php">Sony</a></li>
+  			<li class="list"><a href="userxiomi.php">Xiaomi</a></li>
+  			<li class="list"><a href="useroppo.php">Oppo</a></li>
+  			<div class="cart">
+  			<li class="list"><a href="#about"><i class="fas fa-shopping-cart"></i></a></li>
+  		</div>
+  		<form  class="search" action="/action_page.php">
+  			<li class="Search"><a href="#about"><input type="text" name="search" placeholder="Search.."></a></li>
+  		</form>
+  				
+  	</div>
+	</ul>
+		</nav>
 	<article>
-	<div class="flex-container">
-			<div class="oppof1s"><a href="oppof1s.php"><img src="">Oppo F1s</a><br></div>
-			<div class="oppoa37"><a href="oppoa37.php"><img src="">Oppo A37</a><br></div>
+		<p class="brand">Oppo Product</p>
+	<div class="flex-container2">
+			<div class="oppof1s"><p>Oppo F1S</p><a href="oppof1s.php"><img src="">Buy</a><br></div>
+			<div class="oppoa37"><p>Oppo A37</p><a href="oppoa37.php"><img src="">Buy</a><br></div>
 	</div>
 </article>
 </body>
