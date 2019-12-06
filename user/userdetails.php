24d89ed16c3acd2bcd60727a0ee47cd55dd95838
warniberjaya <58213344+warniberjaya@users.noreply.github.com>
2019-12-07 00:47:24 +0800

    Update userdetails.php

--- user/userdetails.php
+++ user/userdetails.php
@@ -4,6 +4,40 @@
 	<title>Mi Store Update Details</title>
 </head>
 <body>
+	<header><!--top navigation-->
+		<title>HomePage</title>
+		<link rel="stylesheet" type="text/css" href="indexdeis.css">
+	</header>
+	<nav>
+	<!--side navigation-->
+	<section>
+		<nav>
+	<ul id="navitop" class ="navitop">
+		<div class="flex-container">
+			
+		    <li class="dropdown">
+		     <a class="dropbtn">More</a>
+		    <div class="dropdown-content">
+		    <a href="userdetails.php">User Details</a>
+			<a href="index.php">Sign Out</a>
+
+		</div></li> 
+		<li class="list"><a href="#Mistore">Mi Store</a></li>
+  			<li class="list"><a href="userapple.php">Apple</a></li>
+  			<li class="list"><a href="usersamsung.php">Samsung</a></li>
+  			<li class="list"><a href="usersony.php">Sony</a></li>
+  			<li class="list"><a href="userxiomi.php">Xiaomi</a></li>
+  			<li class="list"><a href="useroppo.php">Oppo</a></li>
+  			<li class="list"><a href="usercart.php">Cart</a></li>
+  		<form action="/action_page.php">
+  			<li class="Search"><a href="#about"><input type="text" name="search" placeholder="Search.."></a></li>
+  		</form>
+  				
+  	</div>
+	</ul>
+		</nav>
+	</section>
+<article>		
 <div class="form">
   
 	<h1> Update Details</h1>
@@ -70,11 +104,12 @@
 				
 				<button type="reset" name="resetbtn" class="button"value="reset the form">Clear</button>
 
-				<button type="submit"name="submit"class="button"value="submited">Update<a href="userdetails.html"></button></a><br><br>
+				<button type="submit"name="submit"class="button"value="submited">Update<a href="userdetails.php"></button></a><br><br>
 
-				<button class="btnuserpage"><a href="userdetails.html">Cancel</a></button>
+				<button class="btnuserpage"><a href="userdetails.php">Cancel</a></button>
 			
 		</div>
-
+			</article>
+	<footer>MiStotre</footer>
 </body>
 </html>
