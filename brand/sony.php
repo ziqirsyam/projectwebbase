bb1e0ba7b54a807530824eb6d6333245b391fbd5
acit1199 <58203145+acit1199@users.noreply.github.com>
2019-12-06 18:30:41 +0800

    Update sony.php

--- brand/sony.php
+++ brand/sony.php
@@ -2,14 +2,45 @@
 <html>
 <head>
 	<title>Sony</title>
-</body>
+	<link rel="stylesheet" type="text/css" href="branddeis.css">
+	<script src="https://kit.fontawesome.com/a076d05399.js"></script>
 </head>
 <body>
+		<nav>
+	<!--side navigation-->
+	<section>
+		<nav>
+	<ul id="navitop" class ="navitop">
+		<div class="flex-container">
+			
+		    <li class="dropdown">
+		    <i  id="bars" class="fas fa-bars"></i>
+		    <div class="dropdown-content">
+			<a class="sign-up" href="#">User detail</a>
+     		<a class="log-in" href="#">Sign Out</a>
+		</div></li>
+		<li class="list"><a href="#Mistore">Mi Store</a></li>
+  			<li class="list"><a href="userapple.php">Apple</a></li>
+  			<li class="list"><a href="usersamsung.php">Samsung</a></li>
+  			<li class="list"><a href="usersony.php">Sony</a></li>
+  			<li class="list"><a href="userxiomi.php">Xiaomi</a></li>
+  			<li class="list"><a href="useroppo.php">Oppo</a></li>
+  			<div class="cart">
+  			<li class="list"><a href="#about"><i class="fas fa-shopping-cart"></i></a></li>
+  		</div>
+  		<form  class="search" action="/action_page.php">
+  			<li class="Search"><a href="#about"><input type="text" name="search" placeholder="Search.."></a></li>
+  		</form>
+  				
+  	</div>
+	</ul>
+		</nav>
 <article>
-	<div class="flex-container">
-			<div class="xperiap"><a href="xperiap.php"><img src="">Sony Xperia P</a><br></div>
-			<div class="xperiax"><a href="xperiax.php"><img src="">Sony Xperia X </a><br></div>
-			<div class="xperiaxz"><a href="xperiaxz.php"><img src="">Sony Xperia XZ Premium </a><br></div>
+	<p class="brand">Sony Product</p>
+	<div class="flex-container2">
+			<div class="xperiap"><p>Sony Xperia P</p><a href="xperiap.php"><img src="">Buy</a><br></div>
+			<div class="xperiax"><p>Sony Xperis X</p><a href="xperiax.php"><img src="">Buy</a><br></div>
+			<div class="xperiaxz"><p>Sony Xperis XZ Premium</p><a href="xperiaxz.php"><img src="">Buy</a><br></div>
 	</div>
 </article>
 </body>
